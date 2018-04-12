76d20bd75182ed0db59fb68434c5646ad517fa6f
Joyce Chun <wooyangee@gmail.com>
2018-04-12 22:46:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,12 @@
 # Your code goes here!
 class Dog 
+  
+  def initialize(name, bark)
+    @name = dog_name
+    @bark = barking
+  end
+  
+  def barking()
   def name(dog_name)
     @name = dog_name
   end
@@ -11,8 +18,5 @@ class Dog
   def name
     @name
   end
-  
-  def bark(bark_sound)
-    puts "woof!"
-  end
+
 end
