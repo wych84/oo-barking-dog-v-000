f0f59703a7ff45c5db1fe72684f75e910d04f5c1
Joyce Chun <wooyangee@gmail.com>
2018-04-12 22:57:12 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Dog 
+  
+  def name(dog_name)
+    @name = dog_name
+  end
+  
+  def name=(dog_name)
+    @name = dog_name
+  end
+  
+  def name
+    @name
+  end
+
+  def bark(bark_sound)
+    @bark = bark_sound
+  end
+  
+  def bark
+    puts "woof!"
+  end 
+  
+end
